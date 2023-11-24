4abbe3b6ce8191c1ced38a937094595c24ad6fdf
Pawan Kumar <pk89844@gmail.com>
2023-11-24 11:45:17 +0530

    file update

--- bin/operation.dart
+++ bin/operation.dart
@@ -10,7 +10,18 @@ class FileOperation {
       String filePath = '$currentDirectory/bin/$fileName';
       File file = File(filePath);
       if (!file.existsSync()) {
-        throw Exception('File not found: $filePath');
+        file.createSync(recursive: true);
+        file.writeAsStringSync(jsonEncode({
+          "id": 1,
+          "userId": 1,
+          "date": "2020-03-02T00:00:00.000Z",
+          "products": [
+            {"productId": 1, "quantity": 4},
+            {"productId": 2, "quantity": 1},
+            {"productId": 3, "quantity": 6}
+          ],
+          "__v": 0
+        }));
       }
 
       String contents = file.readAsStringSync();
